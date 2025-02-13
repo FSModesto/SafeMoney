a6e9eb7875aa33fc80495b1d74f8b44f38fda42a
Fernanda Tomaz <fernanda.tomaz@bliteti.onmicrosoft.com>
2025-02-13 14:39:34 -0300

    Classe de usuario e implementação do swagger adicionados

---
+++ SafeMoney/Configurations/SwaggerGenSetup.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+
+namespace SafeMoneyAPI.Configurations
+{
+    public static class SwaggerGenSetup
+    {
+        public static void AddSwaggerGenSetup(this IServiceCollection services)
+        {
+            ArgumentNullException.ThrowIfNull(services);
+
+            services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc("v1", new OpenApiInfo { Title = "SafeMoneyAPI", Version = "v1" });
+                opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "bearer"
+                });
+
+                opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type=ReferenceType.SecurityScheme,
+                                Id="Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
+            });
+        }
+    }
+}
--- SafeMoney/Program.cs
+++ SafeMoney/Program.cs
@@ -1,6 +1,28 @@
+using SafeMoneyAPI.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Configuração do appsettings.json
+builder.Configuration.AddJsonFile("appsettings.json");
+
+// Configurações customizadas
+builder.Services.AddAuthorization();
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGenSetup();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configuração de middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
